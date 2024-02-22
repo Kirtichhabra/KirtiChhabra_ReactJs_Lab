4acd9f17db0170b210309ce953a28ab972112f57
unknown <kirtichhabra99@gmail.com>
2024-02-22 22:45:43 +0530

    Add expense tracker 2 folder

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { ExpenseTrackerApp } from './components/ExpenseTrackerApp';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      {
+        <ExpenseTrackerApp></ExpenseTrackerApp>
+      }
+    </div>
+  );
+}
+
+export default App;
